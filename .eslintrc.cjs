b2b52fa5d982e8e66bba3f46fccf15a0b32aaa5f
makarella-v <v.makarenko@svitla.com>
2024-05-11 13:38:17 +0300

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+    "import/prefer-default-export": "off",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+  },
+};
---
+++ README.md
@@ -0,0 +1,23 @@
+# How to use this template
+
+### Install [degit](https://github.com/Rich-Harris/degit)
+```
+npm install -g degit
+```
+
+### Clone this template
+```
+degit makarenko-v/react-ts-simple-template
+```
+
+### Install dependencies
+```
+npm install
+```
+
+### Run the dev server
+```
+npm run dev
+```
+
+### Enjoy!
